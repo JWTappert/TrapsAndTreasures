1e1c013afccfca36f42e727e63ea942467106340
JWTappert <tappertj@oregonstate.edu>
2017-04-22 19:37:24 -0700

    Revising start screen layout

---
+++ TrapsAndTreasures/TrapsAndTreasures/SplashScreenController.swift
@@ -0,0 +1,22 @@
+//
+//  SplashScreenController.swift
+//  TrapsAndTreasures
+//
+//  Created by Justin Tappert on 4/22/17.
+//  Copyright © 2017 Justin Tappert. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
