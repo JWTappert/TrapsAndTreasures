161c10a8897604e34c081d19b104b8bb0998bda8
JWTappert <tappertj@oregonstate.edu>
2017-04-27 18:07:35 -0700

    Revised storyboard

--- TrapsAndTreasures/TrapsAndTreasures/GameplayController.swift
+++ TrapsAndTreasures/TrapsAndTreasures/GameplayController.swift
@@ -19,41 +19,15 @@ class GameplayController: UIViewController {
     //uiview variables to control gestures
     //and placements
     @IBOutlet var playerAvatar: UIView?
-    @IBOutlet var tile1: UIView?
-    @IBOutlet var tile2: UIView?
     
     //label variables to assign text and such
     @IBOutlet weak var trapsHeldLabel: UILabel!
     @IBOutlet weak var totalStepsHeld: UILabel!
     
     
-    //unused so far
-    @IBOutlet var tile3: UIView?
-    @IBOutlet var tile4: UIView?
-    @IBOutlet var tile5: UIView?
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //give text to labels
-        //trapsHeldLabel.text = "Traps Held"
-        //totalStepsHeld.text = "110 Steps"
-        player = Player()
-        // Do any additional setup after loading the view, typically from a nib.
-    
-        
-        //possibly useless
-        //give tiles a tap function
-        //tap.addTarget(self, action:"tileTapped")
-        
-        //add gesture to tiles
-        tile1?.addGestureRecognizer(tap)
-        tile2?.addGestureRecognizer(tap)
-        
-        //make sure tiles can be tapped
-        tile1?.isUserInteractionEnabled = true
-        tile2?.isUserInteractionEnabled = true
         
         
     }
